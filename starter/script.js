5511484dd743cee8c558197c23f6d52ab77c3eca
DeniseBarbosa <deniseluvoir@gmail.com>
2022-03-02 15:56:48 -0300

    Corrigindo erro

--- starter/script.js
+++ starter/script.js
@@ -1,18 +1,24 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
+  //Quando não ha entrada..
   if (!guess) {
     document.querySelector('.message').textContent = 'Espaço esta vazio!';
+    //Quando o jogador vence..
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Vc acertou';
+    document.querySelector('.number').textContent = secretNumber;
+
+    // A cor muda quando o jogador ganha e caixa que fica o numero aumenta too
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //Quando a estimativa é muito alta..
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent =
@@ -24,6 +30,8 @@ document.querySelector('.check').addEventListener('click', function () {
         'Fim de jogo para você!!!';
       document.querySelector('.score').textContent = 0;
     }
+
+    //Quando a estimativa é muito baixa
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent =
@@ -36,4 +44,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Comece a adivinhar...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  ducument.querySelector('.number').style.width = '15rem';
 });

682e7f6c7a8a1848da1ae169186d05fc4cde1adc
DeniseBarbosa <deniseluvoir@gmail.com>
2022-03-02 15:48:41 -0300

    Implementando a lógica do game

--- starter/script.js
+++ starter/script.js
@@ -1,9 +1,39 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'Espçao esta vazio!';
+    document.querySelector('.message').textContent = 'Espaço esta vazio!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Vc acertou';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        '(muito quente) MUITO ALTO';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'Fim de jogo para você!!!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        '(muito frio) MUITO BAIXO';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'Fim de jogo para você!!!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });

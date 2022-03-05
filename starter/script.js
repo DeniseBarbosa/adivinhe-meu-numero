55050629038569e4aa567445612b592ca2c3e5ae
DeniseBarbosa <deniseluvoir@gmail.com>
2022-03-05 16:04:26 -0300

    Implementando a pontuação

--- starter/script.js
+++ starter/script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -17,6 +18,10 @@ document.querySelector('.check').addEventListener('click', function () {
     // A cor muda quando o jogador ganha e caixa que fica o numero aumenta too
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+    }
 
     //Quando a estimativa é muito alta..
   } else if (guess > secretNumber) {
